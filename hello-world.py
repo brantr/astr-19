1d9939da0453727bdadb14250c106d86a5ca59ff
Brant Robertson <brantr@users.noreply.github.com>
2022-03-31 11:01:15 -0700

    Added a hello world program.
    This repository is now less boring.

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+#This program will write
+#Hello World!
+
+
+#This function tells the
+#computer to print
+#"Hello World!"
+def PrintHelloWorld():
+    print("Hello World!")
+
+#This defines our main()
+#function for our program
+def main():
+    PrintHelloWorld()
+
+#When we run the program
+#this executes first
+if __name__=="__main__":
+    main()
