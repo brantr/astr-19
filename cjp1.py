e02f8b776fd35b358c99d2dcd7c5142f7d8344d5
Brant Robertson <brantr@users.noreply.github.com>
2025-10-02 15:06:51 -0400

    Coding journal prompt 1 solution

---
+++ cjp1.py
@@ -0,0 +1,22 @@
+# This program
+# will print hello
+# world
+
+# this funciton tells the
+# computer to print
+# "Hello World"
+def PrintHelloWorld(x):
+	print(f"Full name: Brant Robertson")
+	print(f'Pronouns (optional): He/Him')
+	print(f'Favorite movie: Ghost in the Shell')
+	print(f'Favorite food: Burritos, I favor Jalapenos.')
+
+# This defines our main()
+# function for our program
+def main():
+	PrintHelloWorld(x)
+
+# When we run the program
+# this executes first
+if __name__=="__main__":
+	main()
